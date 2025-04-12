577894c4e19a711d4c3f6856f681ae738622f1ab
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 11:26:12 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,36 +1,12 @@
+import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from database import get_settings, add_user
 from plugins.smallcaps_plugin import to_smallcaps
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_command(client, message):
-    # Add user to database
-    add_user(message.from_user.id)
-
-    # Get settings
-    settings = get_settings()
-
-    # Check force-sub
-    if settings["request_fsub"]:
-        for channel in settings["force_sub_channels"]:
-            try:
-                member = await client.get_chat_member(channel, message.from_user.id)
-                if member.status not in ["member", "administrator", "creator"]:
-                    await message.reply(await to_smallcaps("ᴅᴏʏᴀ ᴋᴏʀᴇ ᴘʀᴏᴛʜᴏᴍᴇ ᴀᴍᴀᴅᴇʀ ᴄʜᴀɴᴇʟᴇ ᴊᴏʏᴇɴ ᴋᴏʀᴏ!"))
-                    return
-            except:
-                await message.reply(await to_smallcaps("ᴅᴏʏᴀ ᴋᴏʀᴇ ᴘʀᴏᴛʜᴏᴍᴇ ᴀᴍᴀᴅᴇʀ ᴄʜᴀɴᴇʟᴇ ᴊᴏʏᴇɴ ᴋᴏʀᴏ!"))
-                return
-
-    # Send welcome message
-    welcome_msg = settings["welcome_msg"]
-    buttons = [
-        [InlineKeyboardButton("ᴀʙᴏᴜᴛ ᴍᴇ", callback_data="about")],
-        [InlineKeyboardButton("sᴇᴛᴛɪɴɢs", callback_data="settings")]
-    ]
-    await message.reply_photo(
-        photo="images/welcome.jpg",
-        caption=await to_smallcaps(welcome_msg),
-        reply_markup=InlineKeyboardMarkup(buttons)
-                  )
+    logger.info(f"Received /start command from user {message.from_user.id}")
+    await message.reply(await to_smallcaps("ʜᴇʟʟᴏ! ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴀɴɪᴍᴇ ʟᴏʀᴅ ʙᴏᴛ."))
