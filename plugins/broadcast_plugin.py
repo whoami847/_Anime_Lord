45ee7c663d1011bbfd6327902abf498c5c3f6cc6
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 12:06:17 +0600

    Update broadcast_plugin.py

--- plugins/broadcast_plugin.py
+++ plugins/broadcast_plugin.py
@@ -1,22 +1,37 @@
-from pyrogram import Client, filters
+import logging
 from config import ADMIN_IDS
 from database import get_all_users
 from plugins.smallcaps_plugin import to_smallcaps
 
-@Client.on_message(filters.command("broadcast") & filters.user(ADMIN_IDS))
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 async def broadcast_command(client, message):
-    if len(message.command) < 2:
-        await message.reply(await to_smallcaps("ᴅᴏʏᴀ ᴋᴏʀᴇ ᴇᴋᴛɪ ᴍᴇsᴇᴊ ᴅɪɴ!"))
+    logger.info(f"Received /broadcast command from user {message.from_user.id}")
+    if message.from_user.id not in ADMIN_IDS:
+        await message.reply(await to_smallcaps("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ᴀᴅᴍɪɴs!"))
         return
 
-    broadcast_msg = message.text.split(" ", 1)[1]
-    users = get_all_users()
+    if not message.reply_to_message:
+        await message.reply(await to_smallcaps("ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ ᴍᴇssᴀɢᴇ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ!"))
+        return
 
-    # Broadcast to all users
+    users = get_all_users()
+    success_count = 0
+    failed_count = 0
+
     for user_id in users:
         try:
-            await client.send_message(user_id, await to_smallcaps(broadcast_msg))
-        except:
-            continue
+            await message.reply_to_message.copy(user_id)
+            success_count += 1
+        except Exception:
+            failed_count += 1
 
-    await message.reply(await to_smallcaps("ʙʀᴏᴀᴅᴄᴀsᴛ sᴜᴄᴄᴇssғᴜʟʟʏ ᴄᴏᴍᴘʟᴇᴛᴇᴅ!"))
+    await message.reply(
+        await to_smallcaps(
+            f"ʙʀᴏᴀᴅᴄᴀsᴛ ᴄᴏᴍᴘʟᴇᴛᴇᴅ!\n\n"
+            f"sᴜᴄᴄᴇss: {success_count}\n"
+            f"ғᴀɪʟᴇᴅ: {failed_count}"
+        )
+    )
