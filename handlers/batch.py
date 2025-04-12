c34d797f9c3a899dd248dbb8440e6ac8a4ce90b0
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 12:04:51 +0600

    Update batch.py

--- handlers/batch.py
+++ handlers/batch.py
@@ -1,55 +1,39 @@
-from pyrogram import Client, filters
+import logging
 from config import STORAGE_CHANNEL
-from database import start_batch, add_to_batch, get_batch, clear_batch
 from utils.storage import save_to_storage
+from database import add_batch_file, clear_batch_files, get_batch_files
 from plugins.smallcaps_plugin import to_smallcaps
-from plugins.copyright_warning_plugin import apply_copyright_warning
 
-@Client.on_message(filters.command("batch") & filters.private)
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 async def batch_command(client, message):
-    user_id = message.from_user.id
-    batch_data = get_batch(user_id)
+    logger.info(f"Received /batch command from user {message.from_user.id}")
+    clear_batch_files()  # Clear previous batch
+    await message.reply(await to_smallcaps("ʙᴀᴛᴄʜ ᴍᴏᴅᴇ sᴛᴀʀᴛᴇᴅ! sᴇɴᴅ ᴛʜᴇ ғɪʟᴇs ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴏɴᴇ ʙʏ ᴏɴᴇ. ᴡʜᴇɴ ᴅᴏɴᴇ, ᴜsᴇ /ʙᴀᴛᴄʜ_ᴇɴᴅ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ʟɪɴᴋs."))
 
-    if not batch_data:
-        # Start a new batch
-        start_batch(user_id)
-        await message.reply(await to_smallcaps("ʙᴀᴛᴄʜ sᴛᴀʀᴛᴇᴅ! ғᴀɪʟ ᴘᴀᴛʜᴀɴ, ᴛᴀʀᴘᴏʀ ᴘᴜɴᴏʀᴀʏ /ʙᴀᴛᴄʜ ᴋᴏʀᴏ।"))
-    else:
-        # End the batch
-        await batch_end_command(client, message)
-
-@Client.on_message(filters.media & filters.private)
 async def batch_add_file(client, message):
-    user_id = message.from_user.id
-    batch_data = get_batch(user_id)
-
-    if batch_data:
-        # Add file to batch
-        add_to_batch(user_id, message.id)
-
-async def batch_end_command(client, message):
-    user_id = message.from_user.id
-    batch_data = get_batch(user_id)
-
-    if not batch_data or not batch_data["messages"]:
-        await message.reply(await to_smallcaps("ᴋᴏɴᴏ ғᴀɪʟ ᴘᴀᴛʜᴀɴᴏ ʜᴀʏɴɪ!"))
+    logger.info(f"Received batch_add_file command from user {message.from_user.id}")
+    if not message.media:
+        await message.reply(await to_smallcaps("ᴘʟᴇᴀsᴇ sᴇɴᴅ ᴀ ғɪʟᴇ ᴛᴏ ᴀᴅᴅ ᴛᴏ ᴛʜᴇ ʙᴀᴛᴄʜ!"))
         return
 
-    # Save all files to storage channel
-    message_ids = batch_data["messages"]
-    saved_messages = []
-    for msg_id in message_ids:
-        msg = await client.get_messages(message.chat.id, msg_id)
-        saved_msg = await save_to_storage(client, msg, STORAGE_CHANNEL)
-        saved_messages.append(saved_msg.id)
+    file_message = await save_to_storage(client, message, STORAGE_CHANNEL)
+    add_batch_file(file_message.id)
+    await message.reply(await to_smallcaps("ғɪʟᴇ ᴀᴅᴅᴇᴅ ᴛᴏ ʙᴀᴛᴄʜ! sᴇɴᴅ ᴛʜᴇ ɴᴇxᴛ ғɪʟᴇ ᴏʀ ᴜsᴇ /ʙᴀᴛᴄʜ_ᴇɴᴅ ᴛᴏ ғɪɴɪsʜ."))
 
-    # Generate combo link
-    start_id, end_id = min(saved_messages), max(saved_messages)
-    combo_link = f"https://t.me/c/{str(STORAGE_CHANNEL).replace('-100', '')}/{start_id}-{end_id}"
-    await message.reply(await to_smallcaps(f"ᴄᴏᴍʙᴏ ʟɪɴᴋ: {combo_link}"))
+async def batch_end_command(client, message):
+    logger.info(f"Received /batch_end command from user {message.from_user.id}")
+    batch_files = get_batch_files()
+    if not batch_files:
+        await message.reply(await to_smallcaps("ɴᴏ ғɪʟᴇs ᴡᴇʀᴇ ᴀᴅᴅᴇᴅ ᴛᴏ ᴛʜᴇ ʙᴀᴛᴄʜ!"))
+        return
 
-    # Clear batch
-    clear_batch(user_id)
-
-    # Apply copyright warning
-    await apply_copyright_warning(client, message)
+    links = [
+        f"https://t.me/c/{str(STORAGE_CHANNEL).replace('-100', '')}/{file_id}"
+        for file_id in batch_files
+    ]
+    batch_links_text = await to_smallcaps("ʙᴀᴛᴄʜ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋs:\n\n" + "\n".join(links))
+    await message.reply(batch_links_text)
+    clear_batch_files()
