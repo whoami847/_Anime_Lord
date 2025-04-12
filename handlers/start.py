b11fde63b55c4da14fbc296df0ccc21d5b41d02c
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 12:11:58 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,10 @@
 import logging
-from pyrogram import Client, filters
 from plugins.smallcaps_plugin import to_smallcaps
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-@Client.on_message(filters.command("start") & filters.private)
 async def start_command(client, message):
     logger.info(f"Received /start command from user {message.from_user.id}")
     await message.reply(await to_smallcaps("ʜᴇʟʟᴏ! ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴀɴɪᴍᴇ ʟᴏʀᴅ ʙᴏᴛ."))
