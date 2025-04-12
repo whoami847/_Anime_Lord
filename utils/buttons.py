a5c8d7afbc7a868960edd1ec50e2846fb61e1fde
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 16:20:17 +0600

    Delete utils directory

--- utils/buttons.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def create_buttons(button_data):
-    buttons = []
-    for row in button_data:
-        button_row = [InlineKeyboardButton(text, callback_data=callback) for text, callback in row]
-        buttons.append(button_row)
-    return InlineKeyboardMarkup(buttons)
--- utils/storage.py
+++
@@ -1,4 +0,0 @@
-async def save_to_storage(client, message, storage_channel):
-    # Forward the file to the storage channel
-    forwarded_message = await message.forward(storage_channel)
-    return forwarded_message
