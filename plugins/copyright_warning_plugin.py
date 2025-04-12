2836cebe3ebe6121fe3230b90ecf592150cac862
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 12:08:38 +0600

    Update copyright_warning_plugin.py

--- plugins/copyright_warning_plugin.py
+++ plugins/copyright_warning_plugin.py
@@ -1,11 +1,19 @@
 import asyncio
-from pyrogram import Client, filters
+import logging
 from config import ADMIN_IDS
 from database import get_settings, update_settings
 from plugins.smallcaps_plugin import to_smallcaps
 
-@Client.on_message(filters.command("cws") & filters.user(ADMIN_IDS))
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 async def copyright_warning_command(client, message):
+    logger.info(f"Received /cws command from user {message.from_user.id}")
+    if message.from_user.id not in ADMIN_IDS:
+        await message.reply(await to_smallcaps("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ᴀᴅᴍɪɴs!"))
+        return
+
     if len(message.command) < 2:
         await message.reply(await to_smallcaps("ᴅᴏʏᴀ ᴋᴏʀᴇ ɴᴏᴛᴜɴ ᴄᴏᴘʏʀɪɢʜᴛ ᴡᴀʀɴɪɴɢ ᴍᴇsᴇᴊ ᴅɪɴ!"))
         return
@@ -15,6 +23,7 @@ async def copyright_warning_command(client, message):
     await message.reply(await to_smallcaps("ᴄᴏᴘʏʀɪɢʜᴛ ᴡᴀʀɴɪɴɢ ᴍᴇsᴇᴊ sᴜᴄᴄᴇssғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ!"))
 
 async def apply_copyright_warning(client, message):
+    logger.info(f"Applying copyright warning for message from user {message.from_user.id}")
     settings = get_settings()
     warning_msg = settings["copyright_warning_msg"]
     warning_message = await message.reply(await to_smallcaps(warning_msg))
