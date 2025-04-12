a89d7db5f12a5e1f7444ed5fe1ad31641f72d226
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 10:44:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # API credentials (from @BotFather)
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = "28774737"
+API_HASH = "851190ab85bb0b6dd547fff8e3c35b73"
+BOT_TOKEN = "8018682528:AAEB0W9Ljb8Prfyi9qeVYX93OZzcGBuLEYA"
 
 # MongoDB URL
-MONGO_URL = "mongodb://localhost:27017/"
+MONGO_URL = "mongodb+srv://Anime:Anime@anime.suydbfe.mongodb.net/?retryWrites=true&w=majority&appName=Anime"
 
 # Storage channel ID (where files will be saved)
-STORAGE_CHANNEL = "your_storage_channel_id"
+STORAGE_CHANNEL = "-1002656647177"
 
 # Admin IDs (for admin-only commands)
-ADMIN_IDS = [123456789]  # Replace with your Telegram user ID
+ADMIN_IDS = [7282066033]  # Replace with your Telegram user ID
 
 # Default welcome message
 DEFAULT_WELCOME_MSG = "ʜᴇɪ, ᴀᴍɪ ᴇᴋᴛɪ ᴀᴅᴠᴀɴᴄᴇ ғɪʟᴇ sʜᴀʀᴇ ʙᴏᴛ। ᴀᴍᴀʀ sᴇʀᴀ ғɪᴄʜᴀʀ ʜᴏʟᴏ ᴀᴍɪ ʀᴇQᴜᴇsᴛ ғᴏʀᴄᴇ-sᴜʙ ғɪᴄʜᴀʀ sᴀᴘᴏʀᴛ ᴋᴏʀɪ, ʙɪsᴛᴀʀɪᴛ ᴊᴀɴᴛᴇ ᴋʟɪᴄᴋ ᴋᴏʀᴏ।"
