4286da6849b00297b9a9da7586a06637527d7a57
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 10:29:00 +0600

    Delete handlers/smallcaps_plugin.py

--- handlers/smallcaps_plugin.py
+++
@@ -1,6 +0,0 @@
-def to_smallcaps(text):
-    smallcaps_map = str.maketrans(
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘQʀsᴛᴜᴠᴡxʏᴢᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘQʀsᴛᴜᴠᴡxʏᴢ"
-    )
-    return text.translate(smallcaps_map)
