961c82812999b31f2535853fe603cbe2c2027e67
whoami847 <mrbeastbangla069@gmail.com>
2025-04-12 11:58:55 +0600

    Update about.py

--- handlers/about.py
+++ handlers/about.py
@@ -1,24 +1,33 @@
-from pyrogram import Client, filters
+import logging
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.smallcaps_plugin import to_smallcaps
 
-@Client.on_callback_query(filters.regex("about"))
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 async def about_callback(client, callback_query):
-    about_text = (
-        "ᴀᴍᴀʀ ɴᴀᴍ: ᴀɴɪᴍᴇ ʟᴏʀᴅ\n"
-        "ᴀᴅᴠᴀɴᴄᴇ ғɪᴄʜᴀʀ: (ʟɪɴᴋ)\n"
-        "ᴍᴀʟɪᴋ: ᴋɪɴɢ\n"
-        "ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ 3\n"
-        "ʟɪʙʀᴀʀʏ: ᴘʏʀᴏɢʀᴀᴍ V2\n"
-        "ᴅᴀᴛᴀʙᴀsᴇ: ᴍᴏɴɢᴏ ᴅʙ\n"
-        "ᴅᴇᴠᴇʟᴏᴘᴇʀ: @shidoteshika1"
+    logger.info(f"Received about callback from user {callback_query.from_user.id}")
+    about_text = await to_smallcaps(
+        "ɪ ᴀᴍ ᴀɴɪᴍᴇ ʟᴏʀᴅ ʙᴏᴛ, ᴄʀᴇᴀᴛᴇᴅ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴍᴀɴᴀɢᴇ ᴀɴᴅ sʜᴀʀᴇ ғɪʟᴇs ᴇғғɪᴄɪᴇɴᴛʟʏ!\n\n"
+        "✨ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs:\n"
+        "- ɢᴇɴᴇʀᴀᴛᴇ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋs\n"
+        "- ʙᴀᴛᴄʜ ғɪʟᴇ sʜᴀʀɪɴɢ\n"
+        "- ᴄᴜsᴛᴏᴍɪᴢᴀʙʟᴇ sᴇᴛᴛɪɴɢs\n"
+        "- ᴄᴏᴘʏʀɪɢʜᴛ ᴡᴀʀɴɪɴɢ sᴜᴘᴘᴏʀᴛ\n\n"
+        "ᴄʀᴇᴀᴛᴇᴅ ʙʏ: [ʏᴏᴜʀ ɴᴀᴍᴇ](https://t.me/your_username)"
     )
-    buttons = [
-        [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="back_to_start")],
-        [InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]
-    ]
+
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(await to_smallcaps("ʙᴀᴄᴋ"), callback_data="back_to_start")],
+            [InlineKeyboardButton(await to_smallcaps("ᴄʟᴏsᴇ"), callback_data="close")]
+        ]
+    )
+
     await callback_query.message.edit_media(
         media=InputMediaPhoto("images/about.jpg"),
-        caption=await to_smallcaps(about_text),
-        reply_markup=InlineKeyboardMarkup(buttons)
-  )
+        caption=about_text,
+        reply_markup=reply_markup
+    )
+    await callback_query.answer()
